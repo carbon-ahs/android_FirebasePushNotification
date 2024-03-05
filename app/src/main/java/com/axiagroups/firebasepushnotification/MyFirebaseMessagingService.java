98ba0bbe82bc59088a6d5b369635c5715aa5487d
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-03-05 15:05:17 +0600

    firebase push noti build, not sjowing

--- app/src/main/java/com/axiagroups/firebasepushnotification/MyFirebaseMessagingService.java
+++ app/src/main/java/com/axiagroups/firebasepushnotification/MyFirebaseMessagingService.java
@@ -1,6 +1,7 @@
 package com.axiagroups.firebasepushnotification;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -14,6 +15,14 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void getFirebaseMessage(String title, String body) {
-//https://youtu.be/Df71aDbGbTg?si=q_aXlw-cMF1Bp8lr&t=324
+// https://youtu.be/Df71aDbGbTg?si=q_aXlw-cMF1Bp8lr&t=324
+        NotificationCompat.Builder builder = new NotificationCompat
+                .Builder(this,"notify")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Test Noti")
+                .setContentText("Test noti")
+                .setAutoCancel(true);
+
+//        NotificationCompat
     }
 }
